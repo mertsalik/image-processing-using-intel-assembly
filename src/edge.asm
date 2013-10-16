6fcb58b90a094b6a4c982ea5b5c461070738d43e
Mert Salık <mertsalik.itu@gmail.com>
2013-10-17 01:41:06 +0300

    Bugfix & completed.

--- src/edge.asm
+++ src/edge.asm
@@ -7,29 +7,38 @@ edge:
 	push ebp			
 	mov ebp, esp	
 	mov ebx, [ebp+12]	;for width
+	mov dword[width],ebx
 	mov eax, [ebp+16]	;for height
 	mul ebx				;# of pixels
-	mov ecx, eax		;counter is set
-	xor ebx, ebx		;ebx is cleared
-	add ebx, [ebp+20]	;value is assigned to ebx
+	sub eax, ebx		;top row is substracted from width*height
+	mov ecx, eax		;counter is set without top row
 	mov edx, [ebp+8]	;the address of image is pointed by esp
-	
-loop:
+	add ebx, edx		;width+address
+	xor eax,eax			;eax is cleaned for use of al
+looping:
 	dec ecx				;counter is decremented
-	cmp [edx], ebx;values is assigned to image pixel
-	jg	white
-	mov dword [edx], 0
-ending:	
+	mov al,byte[ebx]	;upper pixel -> pixel[i+width]
+	cmp byte [edx],al	;compare pixel[i] and pixel[i+width]
+	ja	white			;if smaller then jump white
+	mov byte [edx], 0	;else it becomes black
+ending:		
 	inc edx				;next pixel
+	inc	ebx				;next pixel from the above row
 	cmp ecx,0			;if image isnot completed then jump back 
-	jne loop
-	
+	jne looping		
+	mov ecx,[width]		;counter is set for the top row
+inloop:
+	dec ecx				;ebx keeps width and decremented because there is one row at the top
+	mov byte[edx],0		;and all pixels at the top row become black
+	inc edx
+	cmp ecx,0		
+	jne inloop
+				
 	mov esp, ebp		;return procedures
 	pop ebp
 	ret
 white:
-	mov dword [edx], 255
+	mov byte [edx], 255
 	jmp ending
-
 
 
